47ef89efc7c84343a3ff6caf5a1a7f10055fa153
Matthias Wiesner <mw@cloudcontrol.de>
2014-07-07 12:24:23 +0200

    Add twig

--- web/index.php
+++ web/index.php
@@ -2,9 +2,21 @@
 require_once __DIR__.'/../vendor/autoload.php'; 
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app->get('/geoip/{domainName}', function ($domainName) use ($app) {
+    return $app['twig']->render('geoip.twig', array(
+        'data' => geoip_record_by_name($domainName),
+    ));
+})
+->bind('geoip');
 
 $app->get('/', function() use($app) { 
-    return print_r(geoip_record_by_name('php.net'), true); 
+    return "Hello to geoip example - type in your browser http://geoipexample.cloudcontrolled.com/geoip/{domainToCheck}"; 
 });
 
 $app->run(); 
